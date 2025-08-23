9043d07894b51f2afb52bb17d267a71b47dbff15
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-08-23 17:40:10 +0200

    feat: Added controller test and service test

---
+++ src/test/java/com/ase/userservice/controllers/LecturerControllerTest.java
@@ -0,0 +1,68 @@
+package com.ase.userservice.controllers;
+
+import com.ase.userservice.dtos.ExamDto;
+import com.ase.userservice.services.LecturerService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(LecturerController.class)
+public class LecturerControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockitoBean
+  private LecturerService lecturerService;
+
+  private static List<ExamDto> examDtoList;
+
+  @BeforeAll
+  public static void setup() {
+    examDtoList = List.of(ExamDto.builder()
+        .name("Test")
+        .module("Test")
+        .date(LocalDate.of(2015,10,12))
+        .time(5400)
+        .build());
+  }
+
+  @Test
+  void fetchExams_shouldReturnExamDtos() throws Exception {
+    Mockito.when(lecturerService.fetchExamsByLecturer("john")).thenReturn(List.of());
+    Mockito.when(lecturerService.convertToExamDto(List.of())).thenReturn(examDtoList);
+    mockMvc.perform(get("/api/v1/lecturer/exams")
+            .param("lecturer","john")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].name").value("Test"))
+        .andExpect(jsonPath("$[0].date").value("2015-10-12"))
+        .andExpect(jsonPath("$[0].module").value("Test"))
+        .andExpect(jsonPath("$[0].time").value(5400));
+  }
+
+  @Test
+  void fetchExams_shouldThrowException() throws Exception{
+    Mockito.when(lecturerService.fetchExamsByLecturer(""))
+        .thenThrow(new IllegalArgumentException("Lecturer cannot be empty"));
+
+    mockMvc.perform(get("/api/v1/lecturer/exams")
+            .param("lecturer","")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest())
+        .andExpect(content().string("Lecturer cannot be empty"));
+  }
+}
---
+++ src/test/java/com/ase/userservice/services/LecturerServiceTest.java
@@ -0,0 +1,119 @@
+package com.ase.userservice.services;
+
+import com.ase.userservice.dtos.ExamDto;
+import com.ase.userservice.entities.Exam;
+import com.ase.userservice.entities.user.Lecturer;
+import com.ase.userservice.entities.user.UserType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@SpringBootTest
+public class LecturerServiceTest {
+  @Autowired
+  private LecturerService lecturerService;
+
+  private Lecturer lecturer;
+
+  @BeforeEach
+  public void setUpLecturer() {
+    lecturer = Lecturer.builder()
+        .id(UUID.randomUUID())
+        .email("lecturer@example.com")
+        .type(UserType.LECTURER)
+        .firstName("John")
+        .lastName("Doe")
+        .build();
+  }
+  @Test
+  void fetchExamsByLecturer_shouldGetExams() {
+    DummyData.EXAMS = List.of(Exam.builder()
+        .name("Mathematics Final Exam")
+        .grade(1)
+        .averageGrade(2)
+        .totalPoints(100)
+        .achievedPoints(95)
+        .examType("Written")
+        .date(LocalDate.of(2015, 10, 12))
+        .time(5400)
+        .allowedResources("Calculator, Formula Sheet")
+        .attempt(1)
+        .etcs(5)
+        .room("Room A101")
+        .lecturer(lecturer)
+        .module("Mathe")
+        .build());
+
+    List<Exam> exams = lecturerService.fetchExamsByLecturer("Test");
+    Exam exam = exams.getFirst();
+
+    Assertions.assertThat(exams).isNotEmpty();
+    Assertions.assertThat(exam.getName()).isEqualTo("Mathematics Final Exam");
+    Assertions.assertThat(exam.getGrade()).isEqualTo(1);
+    Assertions.assertThat(exam.getAverageGrade()).isEqualTo(2);
+    Assertions.assertThat(exam.getTotalPoints()).isEqualTo(100);
+    Assertions.assertThat(exam.getAchievedPoints()).isEqualTo(95);
+    Assertions.assertThat(exam.getExamType()).isEqualTo("Written");
+    Assertions.assertThat(exam.getDate()).isEqualTo(LocalDate.of(2015, 10, 12));
+    Assertions.assertThat(exam.getTime()).isEqualTo(5400);
+    Assertions.assertThat(exam.getAllowedResources()).isEqualTo("Calculator, Formula Sheet");
+    Assertions.assertThat(exam.getAttempt()).isEqualTo(1);
+    Assertions.assertThat(exam.getEtcs()).isEqualTo(5);
+    Assertions.assertThat(exam.getRoom()).isEqualTo("Room A101");
+    Assertions.assertThat(exam.getLecturer()).isEqualTo(lecturer);
+    Assertions.assertThat(exam.getModule()).isEqualTo("Mathe");
+  }
+
+  @Test
+  void fetchExamsByLecturer_shouldNotGetExams() {
+    DummyData.EXAMS = List.of();
+
+    List<Exam> exams = lecturerService.fetchExamsByLecturer("Test");
+
+    Assertions.assertThat(exams).isEmpty();
+  }
+
+  @Test
+  void convertToExamDto_shouldConvertExamsToDto() {
+    List<Exam> exams = List.of(Exam.builder()
+        .name("Mathematics Final Exam")
+        .grade(1)
+        .averageGrade(2)
+        .totalPoints(100)
+        .achievedPoints(95)
+        .examType("Written")
+        .date(LocalDate.of(2015, 10, 12))
+        .time(5400)
+        .allowedResources("Calculator, Formula Sheet")
+        .attempt(1)
+        .etcs(5)
+        .room("Room A101")
+        .lecturer(lecturer)
+        .module("Mathe")
+        .build());
+
+    List<ExamDto> examDtos = lecturerService.convertToExamDto(exams);
+    ExamDto examDto = examDtos.getFirst();
+
+    Assertions.assertThat(examDtos).isNotEmpty();
+    Assertions.assertThat(examDto.getName()).isEqualTo("Mathematics Final Exam");
+    Assertions.assertThat(examDto.getModule()).isEqualTo("Mathe");
+    Assertions.assertThat(examDto.getDate()).isEqualTo(LocalDate.of(2015, 10, 12));
+    Assertions.assertThat(examDto.getTime()).isEqualTo(90);
+    Assertions.assertThat(examDto.getCount()).isEqualTo(1);
+  }
+
+  @Test
+  void convertToExamDto_shouldGetEmptyList() {
+    List<Exam> exams = List.of();
+
+    List<ExamDto> examDtos = lecturerService.convertToExamDto(exams);
+
+    Assertions.assertThat(examDtos).isEmpty();
+  }
+}
