cc04465277f114b6d0969f3c6b8c9466c1281cd4
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-08-27 16:56:49 +0200

    fix: imports and config for imports

--- src/main/java/com/ase/lecturerservice/controllers/LecturerController.java
+++ src/main/java/com/ase/lecturerservice/controllers/LecturerController.java
@@ -1,5 +1,6 @@
 package com.ase.lecturerservice.controllers;
 
+import static com.ase.lecturerservice.controllers.BaseController.BASE_PATH;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ase.lecturerservice.dtos.ExamDto;
 import com.ase.lecturerservice.entities.Exam;
 import com.ase.lecturerservice.services.LecturerService;
-import static com.ase.lecturerservice.controllers.BaseController.BASE_PATH;
 import lombok.RequiredArgsConstructor;
 
 @RestController
