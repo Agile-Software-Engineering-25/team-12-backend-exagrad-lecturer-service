f7996525e17ed402edcfb4847fbc90416f3628f1
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-08-27 16:08:03 +0200

    fix: go rid of jakarta

--- src/main/java/com/ase/lecturerservice/entities/Exam.java
+++ src/main/java/com/ase/lecturerservice/entities/Exam.java
@@ -3,16 +3,8 @@ package com.ase.lecturerservice.entities;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
-import org.hibernate.annotations.UuidGenerator;
 import com.ase.lecturerservice.entities.user.Lecturer;
 import com.ase.lecturerservice.entities.user.Student;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,53 +17,35 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Exam {
-  @Id
-  @UuidGenerator
   protected UUID id;
 
-  @Column(name = "name")
   protected String name;
 
-  @Column(name = "grade")
   protected int grade;
 
-  @Column(name = "average_grade")
   protected int averageGrade;
 
-  @Column(name = "total_points")
   protected int totalPoints;
 
-  @Column(name = "achieved_points")
   protected int achievedPoints;
 
-  @Column(name = "exam_type")
   protected String examType;
 
-  @Column(name = "date")
   protected LocalDate date;
 
-  @Column(name = "time")
   protected int time;
 
-  @Column(name = "allowed_resources")
   protected String allowedResources;
 
-  @Column(name = "attempt")
   protected int attempt;
 
-  @Column(name = "etcs")
   protected int etcs;
 
-  @Column(name = "room")
   protected String room;
 
-  @Column(name = "module")
   protected String module;
 
-  @ManyToOne(cascade = CascadeType.PERSIST)
-  @JoinColumn(name = "lecturer_id")
   protected Lecturer lecturer;
 
-  @ManyToMany(mappedBy = "assignedExams", fetch = FetchType.LAZY)
   protected List<Student> assignedStudents;
 }
--- src/main/java/com/ase/lecturerservice/entities/user/UserEntity.java
+++ src/main/java/com/ase/lecturerservice/entities/user/UserEntity.java
@@ -3,8 +3,6 @@ package com.ase.lecturerservice.entities.user;
 import java.util.UUID;
 import org.hibernate.annotations.UuidGenerator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,22 +15,17 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @SuperBuilder
 public abstract class UserEntity {
-  @Column(name = "email", insertable = false, updatable = false)
   protected String email;
 
-  @Id
   @UuidGenerator
   private UUID id;
 
-  @Column(name = "first_name", nullable = false)
   @JsonProperty("first_name")
   private String firstName;
 
-  @Column(name = "last_name", nullable = false)
   @JsonProperty("last_name")
   private String lastName;
 
-  @Column(name = "type")
   @JsonProperty("type")
   private UserType type;
 }
