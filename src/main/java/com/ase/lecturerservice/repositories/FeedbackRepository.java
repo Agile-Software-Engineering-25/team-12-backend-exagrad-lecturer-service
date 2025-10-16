1b2fbbb18cf031ba5c617563e36ac9935f3dfbc9
fietensen <fietensen@gmail.com>
2025-10-16 10:54:13 +0200

    wrong import order my oarsch

--- src/main/java/com/ase/lecturerservice/repositories/FeedbackRepository.java
+++ src/main/java/com/ase/lecturerservice/repositories/FeedbackRepository.java
@@ -1,8 +1,8 @@
 package com.ase.lecturerservice.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ase.lecturerservice.entities.Feedback;
-import java.util.List;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, String> {
   List<Feedback> findByExamUuid(String examUuid);
