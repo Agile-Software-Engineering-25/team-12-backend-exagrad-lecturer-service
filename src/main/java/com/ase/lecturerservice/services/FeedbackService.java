369fd14c306fac18a5f67f3e6347774bbc265b1f
finnk <finn@kuppler.net>
2025-10-17 00:19:17 +0200

    Added updateFeedback

--- src/main/java/com/ase/lecturerservice/services/FeedbackService.java
+++ src/main/java/com/ase/lecturerservice/services/FeedbackService.java
@@ -1,15 +1,18 @@
 package com.ase.lecturerservice.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import com.ase.lecturerservice.entities.Exam;
 import com.ase.lecturerservice.entities.Feedback;
 import com.ase.lecturerservice.repositories.FeedbackRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Service
@@ -32,10 +35,6 @@ public class FeedbackService {
     ).toList();
   }
 
-  public List<Feedback> getFeedbackForExam(String examUuid) {
-    return feedbackRepository.findByExamUuid(examUuid);
-  }
-
   // TODO: change this webclient, when the API Endpoint is ready
   public Exam getExam(String uuid) {
     return DummyData.EXAMS.stream()
@@ -48,4 +47,25 @@ public class FeedbackService {
     feedbackRepository.save(feedback);
     log.info("Saving grade with UUID: {}", feedback.getUuid());
   }
+
+  public void updateFeedback(String uuid, Feedback updateFeedback) {
+    Feedback existing = feedbackRepository.findById(uuid)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Feedback not found"));
+
+    existing.setComment(updateFeedback.getComment());
+    existing.setPoints(updateFeedback.getPoints());
+    existing.setGrade(updateFeedback.getGrade());
+    existing.setFileReference(updateFeedback.getFileReference());
+    existing.setGradedAt(LocalDate.now());
+
+    feedbackRepository.save(existing);
+
+    log.info("Feedback {} wurde erfolgreich bearbeitet von Lecturer {} (Student {}, Punkte: {}, Note: {})",
+        uuid,
+        existing.getLecturerUuid(),
+        existing.getStudentUuid(),
+        existing.getPoints(),
+        existing.getGrade());
+  }
+
 }
