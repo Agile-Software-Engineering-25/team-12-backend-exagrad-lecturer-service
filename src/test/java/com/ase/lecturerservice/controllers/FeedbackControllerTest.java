f564397b9f02918fbd59c393b3b81988e1bc26fb
1Ixam <maxschulz2005@t-online.de>
2025-11-11 08:03:06 +0000

    fix: checkstyle

--- src/test/java/com/ase/lecturerservice/controllers/FeedbackControllerTest.java
+++ src/test/java/com/ase/lecturerservice/controllers/FeedbackControllerTest.java
@@ -3,7 +3,6 @@ package com.ase.lecturerservice.controllers;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.nio.charset.StandardCharsets;
